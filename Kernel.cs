50bfbd47858384a847577e3e4e9f7bf26e366d6c
Adam1000 <103221656+Adam1000-code@users.noreply.github.com>
2022-11-13 14:41:44 -0500

    Add files via upload

---
+++ Kernel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+using SynOS.Commands;
+
+namespace SynOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        private CommandManager commandManager;
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Welcome to SynOS.");
+            Console.WriteLine("Please enter a command.");
+            this.commandManager = new CommandManager();
+        }
+
+        protected override void Run()
+        {
+            Console.Write("/S:");
+            String response;
+            String input = Console.ReadLine();
+            response = this.commandManager.processInput(input);
+            Console.WriteLine(response);
+        }
+    }
+}
