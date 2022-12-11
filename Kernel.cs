1ec2afb988ea330018698ed6051517897f9f2d97
Adam1000 <103221656+Adam1000-code@users.noreply.github.com>
2022-12-11 10:24:08 -0500

    Add files via upload

--- Kernel.cs
+++ Kernel.cs
@@ -3,17 +3,30 @@ using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
 using SynOS.Commands;
+using Cosmos.System.FileSystem;
 
 namespace SynOS
 {
     public class Kernel : Sys.Kernel
     {
+
         private CommandManager commandManager;
+        private CosmosVFS vfs;
         protected override void BeforeRun()
         {
-            Console.WriteLine("Welcome to SynOS.");
-            Console.WriteLine("Please enter a command.");
+            this.vfs = new CosmosVFS();
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(this.vfs);
             this.commandManager = new CommandManager();
+            Console.Clear();
+
+            Console.WriteLine(" _______  __   __  __    _  _______  _______");
+            Console.WriteLine("|       ||  | |  ||  |  | ||       ||       |");
+            Console.WriteLine("|  _____||  |_|  ||   |_| ||   _   ||  _____|");
+            Console.WriteLine("| |_____ |       ||       ||  | |  || |_____ ");
+            Console.WriteLine("|_____  ||_     _||  _    ||  |_|  ||_____  |");
+            Console.WriteLine(" _____| |  |   |  | | |   ||       | _____| |");
+            Console.WriteLine("|_______|  |___|  |_|  |__||_______||_______|");
+            Console.WriteLine("Welcome to SynOS. Please enter a command.");
         }
 
         protected override void Run()
